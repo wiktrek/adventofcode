92777b0bee816268c4effc1456ad8f9d956522be
wiktrek <wiktor.janiszewski10@gmail.com>
2023-09-17 20:34:20 +0200

    .

--- 2021/1/part1/src/main.rs
+++ 2021/1/part1/src/main.rs
@@ -1,16 +1,16 @@
 fn main() {
     let file = std::fs::read_to_string("./input.txt").unwrap();
-    let mut prev_num = 100000;
-    let mut larger = 0;
+    let mut data: Vec<i32> = vec![];
     let lines = file.lines();
     for line in lines {
         let num: i32 = line.parse::<i32>().unwrap();
-        if num >= prev_num {
-            larger += 1;
-        }
-        prev_num = num;
-
-        println!("{}", num);
+        data.push(num);
     }
-    println!("{}", larger);
+    println!(
+        "{}",
+        data.iter()
+            .zip(data.iter().skip(1))
+            .filter(|(x, y)| x < y)
+            .count()
+    );
 }
