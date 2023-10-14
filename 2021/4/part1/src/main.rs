3610d567dc100e7e969fbcd65168e9e410fb01c6
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-14 23:58:53 +0200

    4 part 1 not finished

--- 2021/4/part1/src/main.rs
+++ 2021/4/part1/src/main.rs
@@ -1,18 +1,40 @@
+struct Column {
+    one: String,
+    two: String,
+    three: String,
+    four: String,
+    five: String,
+}
 fn main() {
     let file = std::fs::read_to_string("../input.txt").unwrap();
     let lines = file.lines();
     let mut a = false;
-    let mut nums: Vec<i32> = vec![];
+    let mut nums: Vec<&str> = vec![];
+    let cols: Vec<Column> = vec![];
+    let mut j = 0;
+    let mut k = 0;
     for line in lines {
         if a {
             // println!("{} aaaaa", line);
+            if k == 5 {
+                k = 0;
+            }
+            j = 0;
+            for n in nums.iter() {
+                if line.contains(format!(" {} ", n).as_str()) {
+                    // println!("{} {}", line, n);
+                    j += 1;
+                    // println!("{}", j);
+                    if j == 5 {
+                        println!("n: {} line: {}", n, line)
+                    }
+                };
+            }
         } else {
             a = true;
             println!("{}", line);
-            let n: Vec<&str> = line.split(',').collect();
+            nums = line.split(',').collect();
 
-            n.iter()
-                .for_each(|i| nums.push((**i).parse::<i32>().unwrap()));
             println!("{:?}", nums);
         }
     }
