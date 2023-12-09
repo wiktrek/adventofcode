a09719725d72c51b2adcd14fb56b94032f90262e
wiktrek <wiktor.janiszewski10@gmail.com>
2023-12-09 19:56:42 +0100

    day1

--- 2023/1/src/main.rs
+++ 2023/1/src/main.rs
@@ -1,5 +1,36 @@
 fn main() {
-    let file = std::fs::read_to_string("../input.txt").unwrap();
+    part1();
+    part2();
+}
+fn part1() {
+    let file = std::fs::read_to_string("input.txt").unwrap();
+    let lines = file.lines();
+    let mut num = 0;
+    for line in lines {
+        let mut a: String = String::new();
+        for c in line.chars() {
+            if c.is_ascii_digit() {
+                a = format!("{}{}", a, c.to_string().to_owned())
+            };
+        }
+
+        if a.len() > 2 {
+            a = format!(
+                "{}{}",
+                a.chars().next().unwrap(),
+                a.chars().nth(a.len() - 1).unwrap()
+            );
+            // println!("{}", a);
+        } else if a.len() == 1 {
+            a = format!("{}{}", a, a);
+            // println!("{}", a);
+        }
+        num += a.parse::<i32>().unwrap();
+    }
+    println!("{}", num)
+}
+fn part2() {
+    let file = std::fs::read_to_string("input.txt").unwrap();
     let lines = file.lines();
     let mut num = 0;
     for l in lines {
@@ -15,7 +46,7 @@ fn main() {
             .replace("nine", "n9ne");
         // .replace("zero", "0");
 
-        println!("{} {}", l, line);
+        // println!("{} {}", l, line);
         let mut a: String = String::new();
 
         for c in line.chars() {
@@ -30,10 +61,10 @@ fn main() {
                 a.chars().next().unwrap(),
                 a.chars().nth(a.len() - 1).unwrap()
             );
-            println!("{}", a);
+            // println!("{}", a);
         } else if a.len() == 1 {
             a = format!("{}{}", a, a);
-            println!("{}", a);
+            // println!("{}", a);
         }
         num += a.parse::<i32>().unwrap();
     }
