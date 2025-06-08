5cd58eb063cc050ce980966687812dc5812975e4
wiktrek <wiktor.janiszewski10@gmail.com>
2025-06-08 16:59:19 +0200

    2015 day 3 part 1

---
+++ 2015/3/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+func main() {
+	data,err := os.ReadFile("./input.txt")
+
+	check(err)
+	part1(string(data))
+	part2(string(data))
+}
+type House struct {
+	x,y, presents int
+}
+func part1(input string) {
+	sum :=0
+	var presents []House
+	var x,y int
+	for i:=0;i<len(input);i++ {
+		if i == 0 {
+			presents = append(presents, House {
+				x:0,
+				y:0,
+				presents: 1,
+			})
+		}
+		char:= input[i]
+		if char == '>' {
+			x++
+		}
+		if char == '<' {
+			x--
+
+		}
+		if char == '^' {
+			y++
+		}
+		if char == 'v' {
+			y--
+		}
+		a := false;
+		for i:=0;i<len(presents);i++ {
+			if presents[i].x == x && presents[i].y == y {
+				presents[i].presents += 1
+				a = true
+			}
+		}
+		if !a {
+			presents = append(presents, House{
+				x: x,
+				y: y,
+				presents: 1,
+			})
+		}
+	}
+	for i:=0;i<len(presents);i++ {
+		if presents[i].presents >= 1 {
+			sum += 1
+		}
+	}
+	fmt.Println(presents)
+	fmt.Println(sum)
+}
+
+func part2(input string) {
+
+}
+
